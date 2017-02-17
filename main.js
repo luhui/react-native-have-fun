6346d5a5d0550871902c500aa7090608e26b9c5f
陆晖 <luhui@cvte.cn>
2017-02-17 15:40:01 +0800

    generator example

--- main.js
+++ main.js
@@ -15,3 +15,20 @@ export default () => (
     </RouterWithRedux>
   </Provider>
 )
+
+
+function* idMaker() {
+  var index = 0
+  console.log('run?');
+  while (index < 3)
+    yield index++
+  return 999
+}
+
+var gen = idMaker();
+
+console.log('start generator');
+console.log(gen.next()); // 0
+console.log(gen.next().value); // 1
+console.log(gen.next().value); // 2
+console.log(gen.next()); // undefined
