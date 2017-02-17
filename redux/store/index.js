fe44538d7310df4d27d71bef49c3833887a51837
陆晖 <luhui@cvte.cn>
2017-02-17 14:34:26 +0800

    add debuger config

--- redux/store/index.js
+++ redux/store/index.js
@@ -4,12 +4,17 @@ import createSagaMiddleware, {END} from 'redux-saga';
 import sagas from 'sagas';
 
 export default (data = {}) => {
-    const sagaMiddleware = createSagaMiddleware();
-    const middleware = applyMiddleware(sagaMiddleware);
-    const enhancer = compose(middleware, global.reduxNativeDevTools ? global.reduxNativeDevTools() : nope => nope);
-    const store = createStore(reducers, data, enhancer);
-    global.reduxNativeDevTools && global.reduxNativeDevTools.updateStore(store)
-    sagaMiddleware.run(sagas)
-    store.close = () => store.dispatch(END);
-    return store;
+  const sagaMiddleware = createSagaMiddleware()
+  const middleware = applyMiddleware(sagaMiddleware)
+  const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose
+  const enhancer = composeEnhancers(middleware)
+  const store = createStore(reducers, data, enhancer)
+  sagaMiddleware.run(sagas)
+  store.close = () => store.dispatch(END)
+  return store
 }
